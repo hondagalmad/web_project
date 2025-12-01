3438a1864435196587b135dc48395b31ddfaeae0
Nour Elhouda <nourelhouda12290@gmail.com>
2025-12-01 11:34:19 +0200

    pixel ver is updated, old ver is dismissed

--- Explore page (pixel theme)/Explore.js
+++ Explore page (pixel theme)/Explore.js
@@ -4,8 +4,8 @@ for (let i = 0; i < numberOfStars; i++) {
   const star = document.createElement('div');
   star.classList.add('star');
 
-  star.style.top = Math.random() * window.innerHeight + 'px';
-  star.style.left = Math.random() * window.innerWidth + 'px';
+  star.style.top = Math.random() * 100 + '%';
+  star.style.left = Math.random() * 100 + '%';
 
   const size = Math.random() * 3 + 1;
   star.style.width = size + 'px';
